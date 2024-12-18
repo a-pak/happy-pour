c4eb38b0d4d4721ebd707c628166278d1917bd52
a-pak <kuivalainenap@gmail.com>
2024-12-18 17:08:20 +0200

    added location based mapping

--- React-client/happypour/src/components/MapsComponent.tsx
+++ React-client/happypour/src/components/MapsComponent.tsx
@@ -11,23 +11,52 @@ import {
 "use client"
 
 const MapsComponent: React.FC = () => {
-
+  const [userLocation, setUserLocation] = useState<{ lat: number; lng: number } | null>(null);
+  const [error, setError] = useState<string | null>(null);
   const position = { lat: 60.10, lng: 24.55}
-  console.log(import.meta.env.VITE_GOOGLE_MAPS_API_KEY);
+
+
+  useEffect(() => {
+    // Hakee käyttäjän sijainnin, kun komponentti on ladattu
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
+          setUserLocation({ lat: latitude, lng: longitude });
+        },
+        (err) => {
+          setError('Sijainnin haku epäonnistui. Salli sijainnin käyttö selaimen asetuksista.');
+          console.error(err);
+        }
+      );
+    } else {
+      setError('Sijainnin haku ei ole tuettu tässä selaimessa.');
+    }
+  }, []);
+
 
   return (
     <div>
-      <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
+      {error ? (<p>{error}</p>)
+      : !userLocation ? ( <p>Ladataan käyttäjän sijaintia...</p>)
+      : (
+        <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
         <div style={{ height: "100vh" }}>
-          <Map zoom={12} center={position}>
-            <AdvancedMarker position={position}>
-              <Pin />
-            </AdvancedMarker>
+          <Map 
+            zoom={12} 
+            center={userLocation}
+            draggable={true}
+            zoomControl={true}
+
+          >
+
           </Map>
 
         </div>
         
       </APIProvider>
+        )
+      }
     </div>
   );
 }
