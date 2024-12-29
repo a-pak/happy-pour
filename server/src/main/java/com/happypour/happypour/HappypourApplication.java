1618abf9ca4da27a7da9cc0e2bd3b59b88cdd9c2
a-pak <kuivalainenap@gmail.com>
2024-12-29 22:24:58 +0200

    changed BE framework to Spring, created React routes

---
+++ server/src/main/java/com/happypour/happypour/HappypourApplication.java
@@ -0,0 +1,13 @@
+package com.happypour.happypour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappypourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HappypourApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/happypour/happypour/HappypourApplicationTests.java
@@ -0,0 +1,13 @@
+package com.happypour.happypour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappypourApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
