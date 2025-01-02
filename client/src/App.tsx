88122bb2a5f323d59de38de4d2d92f18eee44f22
a-pak <kuivalainenap@gmail.com>
2025-01-02 16:34:08 +0200

    added reeact routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,24 @@
 import './App.css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom'
 
 import AppBarComponent from './components/AppBarComponent'
 import MapsComponent from './components/MapsComponent'
+import { LogInPage } from './pages/LogInPage'
+import BarListPage from './pages/BarListPage'
 
 function App() {
 
   return (
     <>
       <AppBarComponent />
-      <MapsComponent />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MapsComponent />}/>
+          <Route path="/bars" element={<BarListPage />} />
+          <Route path="/login" element={<LogInPage />} />
+        </Routes>
+      </BrowserRouter>
+
     </>
   )
 }
--- server/src/main/java/com/happypour/happypour/repository/BarRepository.java
+++ server/src/main/java/com/happypour/happypour/repository/BarRepository.java
@@ -1,7 +1,8 @@
 package com.happypour.happypour.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import com.happypour.happypour.model.Bar;
 
-public interface BarRepository extends JpaRepository {
+public interface BarRepository extends JpaRepository<Long, Bar> {
 
 }
