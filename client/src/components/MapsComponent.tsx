bdfeed314df596dfb0477a993e81f5e842c321fb
a-pak <kuivalainenap@gmail.com>
2025-01-17 14:52:02 +0200

    added marker functionality to submitting new bar from FE

--- client/src/components/MapsComponent.tsx
+++ client/src/components/MapsComponent.tsx
@@ -3,14 +3,19 @@ import { useState, useEffect } from 'react'
 import barsService from '../services/bars'
 import { LocationMarkerComponent } from './LocationMarkerComponent'
 import Bar from '../model/IbarInterface';
-import { MapContainer, Marker, TileLayer } from 'react-leaflet';
+import { MapContainer, Popup, Marker, TileLayer, useMapEvents } from 'react-leaflet';
 import locationService from '../services/location'
 import L from 'leaflet'
+
+interface MapEventsHandlerProps {
+  handleMapClick: (event: L.LeafletMouseEvent) => void;
+}
 
 const MapsComponent: React.FC = () => {
   const [userLocation, setUserLocation] = useState<[number, number] | null>(null);  
   const [bars, setBars] = useState<Bar[] | null>(null);
   const [error, setError] = useState<string | null>(null);
+  const [tempMarker, setTempMarker] = useState<[number, number] | null>(null);
 
   ////////// Asks the location of the user using locationService
   useEffect(() => {
@@ -39,33 +44,72 @@ const MapsComponent: React.FC = () => {
       })
   }, [navigator.geolocation])
 
+  ////////////////Add bar to map
+
+  const handleMapClick = (e: L.LeafletMouseEvent) => {
+    const { lat, lng } = e.latlng;
+    setTempMarker([lat, lng]); 
+    //alert(`Clicked at: ${lat}, ${lng}`);
+  };
+
+  const MapEventsHandler: React.FC<MapEventsHandlerProps> = ({ handleMapClick }) => {
+    useMapEvents({
+      dblclick: (e) => handleMapClick(e),
+    });
+    return null;
+  };
+
+  
+  const openAddBarWindow = () => {
+    if (tempMarker) {
+      const [lat, lng] = tempMarker;
+      // Redirect to submit
+      const addBarUrl = `/add-bar?lat=${lat}&lng=${lng}`;
+      window.open(addBarUrl, '_blank');
+    }
+  };
+
+
   return (
-    <div>
-        
-            <div id='map' style={{ height: `calc(100vh - 56px)` }}>
-            <MapContainer 
-              center={userLocation || [60.192059, 24.945841]}                
-              zoom={13} 
-              className="leaflet-container"
-            >
-            <TileLayer
-              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            />
-            {userLocation && (
-              <Marker zIndexOffset={1000} position={userLocation} icon={L.icon({
-                iconUrl: "/user.png",
-                iconSize: [40, 40],
-                iconAnchor: [20, 20],
-              })}
-              >
-              </Marker>
-            )}
-            {error ? (<p>{error}</p>)
-              :<LocationMarkerComponent bars={bars} />
-            }
-          </MapContainer>
-          </div>
+    <div>        
+      <div id='map' style={{ height: `calc(100vh - 56px)` }}>
+        <MapContainer 
+          center={userLocation || [60.192059, 24.945841]}                
+          zoom={13} 
+          className="leaflet-container"
+        >
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        />
+        <MapEventsHandler handleMapClick={handleMapClick}/>
+
+        {userLocation && (
+          <Marker zIndexOffset={1000} position={userLocation} icon={L.icon({
+            iconUrl: "/user.png",
+            iconSize: [40, 40],
+            iconAnchor: [20, 20],
+          })}
+          >
+          </Marker>
+        )}
+
+        {tempMarker && (
+          <Marker position={tempMarker}>
+            <Popup>
+              <div>
+                <p>Do you want to add a new bar to this Location?</p>
+                <button onClick={openAddBarWindow}>Add bar</button>
+              </div>
+            </Popup>
+          </Marker>
+        )}
+
+        {error ? (<p>{error}</p>)
+          :<LocationMarkerComponent bars={bars} />
+        }
+      </MapContainer>
+      </div>
 
     </div>
   );
