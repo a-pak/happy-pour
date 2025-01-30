b22820ec0a5ce8dff8a84a55dd6797bd40114105
a-pak <kuivalainenap@gmail.com>
2025-01-30 11:05:19 +0200

    added cloud database connection to project.

--- server/src/main/java/com/happypour/happypour/HappypourApplication.java
+++ server/src/main/java/com/happypour/happypour/HappypourApplication.java
@@ -19,17 +19,14 @@ public class HappypourApplication{
 		SpringApplication.run(HappypourApplication.class, args);
 	}
 
-	// Voit käyttää CommandLineRunnerin avulla testidatan lisäämistä
     @Bean
     public CommandLineRunner demo(BarRepository repository) {
         return (args) -> {
             // Lisää testidataa
-            repository.save(new Bar(null, "Bar Helsinki", 24.9384, 60.1699, "Mannerheimintie 1", 5.50, 7.20, 3.00, 10.00, 2.50));
-            repository.save(new Bar(null, "The Kallio Pub", 24.9484, 60.1799, "Pasilantie 2", 6.00, 8.00, 3.50, 8.00, 3.00));
-            repository.save(new Bar(null, "Riverside Bar", 24.9334, 60.1649, "Itämerenkatu 3", 5.80, 7.50, 4.00, 12.00, 3.00));
+            // repository.save(new Bar(null, "Bar Helsinki", 24.9384, 60.1699, "Mannerheimintie 1", 5.50, 7.20, 3.00, 10.00, 2.50));
+            // repository.save(new Bar(null, "The Kallio Pub", 24.9484, 60.1799, "Pasilantie 2", 6.00, 8.00, 3.50, 8.00, 3.00));
+            // repository.save(new Bar(null, "Riverside Bar", 24.9334, 60.1649, "Itämerenkatu 3", 5.80, 7.50, 4.00, 12.00, 3.00));
 
-
-            // Tulosta kaikki baarit
             repository.findAll().forEach(bar -> System.out.println(bar));
         };
     }
