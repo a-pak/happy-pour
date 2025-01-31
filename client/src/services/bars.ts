73091f353ec870c462d223a7448e4ecceece129a
opoussa <poussa.otso@gmail.com>
2025-01-31 12:14:38 +0200

    WIP: re-add update function to service.
    Also removed merge conflict files

--- client/src/services/bars.ts
+++ client/src/services/bars.ts
@@ -1,26 +1,31 @@
 import axios from 'axios'
-//const baseUrl = 'http://localhost:8080/api/bars';
-const baseUrl = 'https://happy-pour-be.onrender.com/api/bars';
 import Bar from '../model/IbarInterface';
 
+const BASE_URL : string = import.meta.env.VITE_BARS_URL;
 
 const getAll = async (): Promise<Bar[]> => {
-    const response = await axios.get(baseUrl);
+    const response = await axios.get(BASE_URL);
     return response.data;
 }
 
 const create = async (newBar: Bar): Promise<Bar> => {
-    const response = await axios.post<Bar>(baseUrl, newBar);
+    const response = await axios.post<Bar>(BASE_URL, newBar);
+    return response.data;
+}
+
+const update = async (newBar: Bar): Promise<Bar> => {
+    const response = await axios.put<Bar>(BASE_URL, newBar);
     return response.data;
 }
 
 const getById = async (id: Number): Promise<Bar> => {
-    const response = await axios.get(`${baseUrl}/${id}`);
+    const response = await axios.get(`${BASE_URL}/${id}`);
     return response.data;
 }
 
 export default {
     getAll,
     create,
+    update,
     getById
 }
