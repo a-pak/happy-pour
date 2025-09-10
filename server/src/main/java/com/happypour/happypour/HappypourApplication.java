48d665cb54a42b13c92e0113d5d74c61bc8c109c
opoussa <poussa.otso@gmail.com>
2025-09-10 17:46:34 +0300

    Clear unused imports in java main

--- server/src/main/java/com/happypour/happypour/HappypourApplication.java
+++ server/src/main/java/com/happypour/happypour/HappypourApplication.java
@@ -1,19 +1,7 @@
 package com.happypour.happypour;
 
-import com.happypour.happypour.dto.RegisterRequest;
-import com.happypour.happypour.model.*;
-import com.happypour.happypour.model.embeddable.HappyHourDrinkId;
-import com.happypour.happypour.repository.*;
-
-import java.time.LocalTime;
-
-import com.happypour.happypour.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationRunner;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class HappypourApplication {
