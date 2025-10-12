cf3b7593f08b31799d86b9c5248c137dc7a92c5d
opoussa <poussa.otso@gmail.com>
2025-10-12 23:22:49 +0300

    NEW DRINK API: Refactor services

--- client/src/services/happyhours.ts
+++ client/src/services/happyhours.ts
@@ -1,14 +1,21 @@
-import HappyHour from '../model/IHappyHourInterface'
+import {HappyHourDTO} from '../model/IHappyHourInterface'
 import api from '../utils/axiosInstance';
 
 const HAPPY_HOUR_URL : string = "/happyhours";
 
 export const getHappyHour = async (id : number) => {
-    const response = await api.get<HappyHour>(HAPPY_HOUR_URL + `/${id}`);
+    const response = await api.get(HAPPY_HOUR_URL + `/${id}`);
     return response.data;
 }
 
-export const createHappyHour = async (happyHour: HappyHour) => {
-    const response = await api.post<HappyHour>(HAPPY_HOUR_URL, happyHour);
+export const createHappyHour = async (happyHour: HappyHourDTO) => {
+    console.log(happyHour);
+    const response = await api.post(HAPPY_HOUR_URL, happyHour);
+    return response.data;
+}
+
+export const updateHappyHour = async (happyHour: HappyHourDTO) => {
+    console.log(happyHour);
+    const response = await api.put(HAPPY_HOUR_URL, happyHour);
     return response.data;
 }
