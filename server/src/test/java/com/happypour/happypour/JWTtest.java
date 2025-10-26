8befb378aa27c8681357cf8e6a8c632abbf46e91
opoussa <poussa.otso@gmail.com>
2025-10-26 09:32:59 +0200

    Remove unused import

--- server/src/test/java/com/happypour/happypour/JWTtest.java
+++ server/src/test/java/com/happypour/happypour/JWTtest.java
@@ -3,7 +3,6 @@ package com.happypour.happypour;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
