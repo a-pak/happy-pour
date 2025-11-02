9ed6618bcb1c6c60321deb069f5d8e06eec4cbaa
opoussa <poussa.otso@gmail.com>
2025-11-02 22:12:16 +0200

    Extend bar query location to 8km.

--- server/src/main/java/com/happypour/happypour/repository/BarRepository.java
+++ server/src/main/java/com/happypour/happypour/repository/BarRepository.java
@@ -16,12 +16,13 @@ public interface BarRepository extends JpaRepository<Bar, Long> {
     @Query("delete from Bar b where b.id= ?1")
     void deleteById(long id);
 
-    /** <h4>findByLocation</h4> looks for bars in a 3km distances from given location.
+    /** <h4>findByLocation</h4> looks for bars in an 8 km distance from the given location.
      *
-     * @return List of Bars 3km distance from given location
+     * @return List of Bars 8 km distance from the given location
      */
-    @Query("SELECT Bar FROM Bar b " +
-            "WHERE b.coordLat BETWEEN ?1 - 0.01795 AND ?1 + 0.01795" +
-            "AND b.coordLong BETWEEN ?1 - 0.01795 AND ?1 + 0.01795")
-    List<Bar> findByLocation(double Lat, double Long);
+    @Query("SELECT b FROM Bar b " +
+        "WHERE b.coordLat BETWEEN ?1 - 0.072 AND ?1 + 0.072 " +
+        "AND b.coordLong BETWEEN ?2 - 0.072 AND ?2 + 0.072")
+    List<Bar> findByLocation(double lat, double lon);
+
 }
