0c7face9b6f60abd49ad02bbd596b2618c40b0f2
opoussa <poussa.otso@gmail.com>
2026-03-21 22:05:49 +0200

    create time util for frontend

--- client/src/utils/timeUtils.ts
+++ client/src/utils/timeUtils.ts
@@ -5,5 +5,5 @@ export const formatTimestamp = (timestamp: number): string => {
   const year = String(date.getFullYear()).slice(-2);
   const hours = String(date.getHours()).padStart(2, '0');
   const minutes = String(date.getMinutes()).padStart(2, '0');
-  return `${day}:${month}:${year}:${hours}:${minutes}`;
+  return `${day}/${month}/${year} ${hours}:${minutes}`;
 };
