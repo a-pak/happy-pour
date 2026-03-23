1afba8c4d4455c41e0c8c6754ce60b8281f62d12
opoussa <poussa.otso@gmail.com>
2026-03-23 22:31:35 +0200

    Save happy hour time only in minutes

--- server/src/main/java/com/happypour/happypour/mapper/HappyHourMapper.java
+++ server/src/main/java/com/happypour/happypour/mapper/HappyHourMapper.java
@@ -1,5 +1,6 @@
 package com.happypour.happypour.mapper;
 
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,22 +14,16 @@ import com.happypour.happypour.entity.User;
  * Mapper for Happy Hours. Includes functions for mapping database entity to http-layer DTO and DTO to database entity.
  */
 public class HappyHourMapper {
-    // Helper method to convert a list of Price entities to a list of PriceDTO records
-    private static List<PriceDTO> mapPrices(List<Price> prices) {
-        if (prices == null)
-            return new ArrayList<PriceDTO>();
-
-        return prices.stream()
-                     .map(PriceMapper::toDTO)
-                     .toList();
-    }
     /**
      * Maps happy hour entity to a happy hour  DTO.
      * @param happyHour entity
      * @param associatedPrices List of associated Price entities
      * @return Happy hour dto from given happy hour entity.
      */
-    public static HappyHourDTO toDTO(HappyHour happyHour, List<Price> associatedPrices) {
+    public static HappyHourDTO toDTO(
+        HappyHour happyHour, 
+        List<Price> associatedPrices
+    ) {
         String createdByUsername = happyHour.getCreatedBy() != null ? happyHour.getCreatedBy().getUsername() : null;
         String updatedByUsername = happyHour.getUpdatedBy() != null ? happyHour.getUpdatedBy().getUsername() : null;
         Long creatorId = happyHour.getCreatedBy() != null ? happyHour.getCreatedBy().getId() : null;
@@ -40,8 +35,10 @@ public class HappyHourMapper {
         return new HappyHourDTO(
             happyHour.getId(),
             happyHour.getWeekDays(), 
-            happyHour.getStartTime(),
-            happyHour.getEndTime(),
+            happyHour.getStartTime()
+                .truncatedTo(ChronoUnit.MINUTES),
+            happyHour.getEndTime()
+                .truncatedTo(ChronoUnit.MINUTES),
             barId,
             createdByUsername,
             createdAt,
@@ -51,6 +48,7 @@ public class HappyHourMapper {
             creatorId
         );
     }
+    
     /**
      * Maps happy hour DTO to database entity HappyHour.
      * @param happyHourDTO hh dto
@@ -58,14 +56,32 @@ public class HappyHourMapper {
      * @param bar Associated bar entity
      * @return Happy hour Entity from given dto.
      */
-    public static HappyHour toEntity(HappyHourDTO happyHourDTO, Bar bar, User createdBy) {
+    public static HappyHour toEntity(
+        HappyHourDTO happyHourDTO, 
+        Bar bar, 
+        User createdBy
+    ) {
         return HappyHour.builder()
             .bar(bar)
-            .startTime(happyHourDTO.getStartTime())
-            .endTime(happyHourDTO.getEndTime())
+            .startTime(happyHourDTO.getStartTime()
+                .truncatedTo(ChronoUnit.MINUTES)
+            )
+            .endTime(happyHourDTO.getEndTime()
+                .truncatedTo(ChronoUnit.MINUTES)
+            )
             .weekDays(happyHourDTO.getWeekDays())
             .createdBy(createdBy)
             .updatedBy(createdBy)
             .build();
+    }
+    
+    // Helper method to convert a list of Price entities to a list of PriceDTO records
+    private static List<PriceDTO> mapPrices(List<Price> prices) {
+        if (prices == null)
+            return new ArrayList<PriceDTO>();
+
+        return prices.stream()
+                     .map(PriceMapper::toDTO)
+                     .toList();
     }
 }
