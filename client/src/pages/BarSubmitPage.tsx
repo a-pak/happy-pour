4d395eb7fe4241943ac5d5ad55324970ac99971f
opoussa <poussa.otso@gmail.com>
2026-04-03 09:27:38 +0300

    Remove redundant barsubmit component

--- client/src/pages/BarSubmitPage.tsx
+++ client/src/pages/BarSubmitPage.tsx
@@ -1,5 +1,24 @@
 import { useParams, useSearchParams } from "react-router-dom";
-import BarSubmitComponent from "../components/BarSubmitComponent.tsx";
+import React, { useEffect, useState } from "react";
+import {
+  TextField,
+  Button,
+  Box,
+  Typography,
+  Paper
+} from "@mui/material";
+import barService from "../services/bars";
+import Bar from "../types/IbarInterface.ts";
+import { LocalizationProvider, TimePicker } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import dayjs, { Dayjs } from "dayjs";
+import { Link, useNavigate } from "react-router-dom";
+import { useUserStore } from "../store/userStore.ts";
+import { useErrorStore } from '../store/errorStore.ts';
+import theme from "../Theme";
+import { getAddress } from "../services/geocode.ts";
+import { ArrowBack } from "@mui/icons-material";
+import { AxiosError } from "axios";
 
 export const BarSubmitPage : React.FC = () => {
   const {barId} = useParams();
@@ -7,16 +26,220 @@ export const BarSubmitPage : React.FC = () => {
   const latParam = searchParams.get('lat');
   const lngParam = searchParams.get('lng');
 
+  const barIdNum = barId ? parseInt(barId) : null;
   const lat = latParam ? parseFloat(latParam) : -1;
   const long = lngParam ? parseFloat(lngParam) : -1;
 
-  return barId ? (
-    <div>
-        <BarSubmitComponent barId={parseInt(barId)} lat={lat} lng={long}/>
-    </div>
-    ) : (
-    <div>
-        <BarSubmitComponent lat={lat} lng={long}/>
-    </div>
-  )
-}
+  const navigate = useNavigate();
+  const { user, setUser } = useUserStore();
+  const { showNotification } = useErrorStore.getState();
+
+  const [name, setName] = useState("");
+  const [address, setAddress] = useState("");
+  const [openFrom, setOpenFrom] = useState<Dayjs | null>(dayjs("14:30", "HH:mm"));
+  const [openTo, setOpenTo] = useState<Dayjs | null>(dayjs("02:00", "HH:mm"));
+  const [latitude, setLatitude] = useState<number>(lat ? lat : 0);
+  const [longitude, setLongitude] = useState<number>(long ? long : 0);
+  //const [entryFee, setEntryFee] = useState(0.0);
+  //const [cloakroomFee, setCloakroomFee] = useState(0.0);
+
+  useEffect(() => {
+    const fetchAddress = async () => {
+      await getAddress(lat, long).then((res) => {
+        
+        if (res) {
+          
+          if (res.status != 200 || !res.data) {
+            console.error('Request failed:', res.status);
+            throw new Error('Reverse geocoding failed');
+          }
+          const data = res.data
+
+          if (data && data.address) {
+            const addr = data.address;
+
+            const road = addr.road || "";
+            const houseNumber = addr.house_number || "";
+            const postcode = addr.postcode || "";
+            const city = addr.city || addr.town || addr.village || "";
+
+            const formattedAddress = [road, houseNumber, postcode, city]
+              .filter(Boolean)
+              .join(" ");
+
+            setAddress(formattedAddress);
+          } else {
+            console.warn("Address not found from Nominatimista.");
+          }
+        }
+      })
+        .catch((err: AxiosError) => {
+          if (err.status === 502 && import.meta.env.DEV) {
+            console.error("502. Nominatim service unavalaible in dev mode.");
+          } else {
+            // TODO : show warning on address field.
+            console.error(`Error fetching address. Status ${err.status}`);
+            console.error(err.message);
+          }
+        });
+    }
+    if (barIdNum) {
+      barService.getById(barIdNum).then((bar) => {
+        setName(bar.bar.name);
+        setAddress(bar.bar.address);
+        setOpenFrom(dayjs(bar.bar.openFrom, "HH:mm"));
+        setOpenTo(dayjs(bar.bar.openTo, "HH:mm"));
+        setLatitude(bar.bar.coordLat);
+        setLongitude(bar.bar.coordLong);
+      });
+    } else {
+      fetchAddress();
+    }
+  }, [barId, lat, long]);
+
+  const handleSubmit = async () => {
+    if (!name || !address || !openFrom || !openTo) {
+      showNotification("Please fill in all fields.", "warning");
+      return;
+    }
+
+    if (!user) {
+      showNotification("You must be logged in to submit a bar.", "warning");
+      navigate("/login");
+      return;
+    }
+
+    const now = Date.now();
+
+    const bar: Bar = {
+      id: barIdNum ?? 0,
+      name,
+      coordLong: longitude,
+      coordLat: latitude,
+      address,
+      openFrom: openFrom.format("HH:mm"),
+      openTo: openTo.format("HH:mm"),
+      //entryFee,
+      //cloakroomFee,
+      createdBy: user.username,
+      updatedBy: user.username,
+      createdAt: now,
+      updatedAt: now,
+      creatorId: user.id
+    };
+    console.log("Submitting bar:", bar);
+    try {
+      if (barIdNum) {
+        console.log("UPDATE");
+        await barService.update(barIdNum, bar);
+        navigate("/bars/" + barIdNum);
+        showNotification("Bar updated successfully!", "success");
+      } else {
+        console.log("CREATE");
+        const response = await barService.create(bar);
+        if (response) {
+          navigate("/bars/" + response.id);
+          showNotification("Bar created successfully!", "success");
+        } else {
+          throw new Error("No bar in response");
+        }
+      }
+    } catch (error: any) {
+      console.error("Error:", error);
+      if (error.status === 401 || error.status === 403) {
+        setUser(null);
+        navigate("/login");
+        showNotification("Session expired. Please log in again.", "warning");
+      } else {
+        showNotification("An error occurred while submitting the form. Please try again.");
+      }
+    }
+  };
+
+  return (
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <Paper
+        elevation={4}
+        sx={{
+          maxWidth: 500,
+          margin: "4vh auto",
+          padding: 4,
+          position: "relative",
+          borderRadius: 4,
+          backgroundColor: theme.palette.background.default,
+        }}
+      >
+        <Button 
+          component={Link} 
+          to={barId ? `/bars/${barId}/details` : "/"} 
+          variant="outlined" sx={{ mb: 2 }} 
+          startIcon={<ArrowBack />} 
+          >Back to {barId ? "Details" : "Map"}
+        </Button>
+
+        <Typography variant="h5" fontWeight={600} gutterBottom>
+          {barId ? "Update Bar" : "Add Bar"}
+        </Typography>
+
+        <Box display="flex" flexDirection="column" gap={2}>
+          <TextField
+            label="Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            fullWidth
+          />
+
+          <TextField // TODO : show warning on address field on Nominatim error.
+            label="Address"
+            value={address}
+            onChange={(e) => setAddress(e.target.value)}
+            fullWidth
+          />
+
+          <TimePicker
+            label="Open from"
+            ampm={false}
+            value={openFrom}
+            onChange={(newValue) => setOpenFrom(newValue)}
+          />
+
+          <TimePicker
+            label="Open to"
+            ampm={false}
+            value={openTo}
+            onChange={(newValue) => setOpenTo(newValue)}
+          />
+          {/*
+          <TextField
+            label="Entry fee (€)"
+            type="number"
+            value={entryFee}
+            onChange={(e) => setEntryFee(parseFloat(e.target.value))}
+          />
+          
+          <TextField
+            label="Cloakroom fee (€)"
+            type="number"
+            value={cloakroomFee}
+            onChange={(e) => setCloakroomFee(parseFloat(e.target.value))}
+          />
+          */}
+          <Button
+            variant="contained"
+            color="secondary"
+            onClick={handleSubmit}
+            sx={{
+              mt: 1,
+              textTransform: "none",
+              fontWeight: 500,
+              borderRadius: 2,
+              boxShadow: "none",
+            }}
+          >
+            {barId ? "Update" : "Create Bar"}
+          </Button>
+        </Box>
+      </Paper>
+    </LocalizationProvider>
+  );
+};
